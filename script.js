b6b036d976bdff361db6fbfbbd7e3c50a9142867
vialee <1574642049@qq.com>
2019-03-01 23:48:13 +0800

    addcss

--- script.js
+++ script.js
@@ -17,23 +17,60 @@ function quitloading(){
 }
 
 function listenScroll () {
-    document.onscroll = function navscroll (xxx) {
-        var topHead = document.getElementById('topHead')
-        let list = document.querySelectorAll('#topHead .navBar li > a')
+    // document.onscroll = function navscroll (xxx) {
+    window.onscroll = function navscroll (xxx) {
+        let targetElement = document.querySelectorAll('[data-x]')
+        let minindex = 0
+        for(var i=1;i<targetElement.length;i++){
+            var x = Math.abs(targetElement[i].offsetTop-window.scrollY)
+            var y = Math.abs(targetElement[minindex].offsetTop-window.scrollY)
+            // console.log()
+            if(x<y){
+                minindex = i
+            }
+            // var top1 = Math.abs(targetElement[0].offsetTop-window.scrollY)
+            // var top2 = Math.abs(targetElement[1].offsetTop-window.scrollY)
+            // var top3 = Math.abs(targetElement[2].offsetTop-window.scrollY)
+            // var x=top1
+            //     if(top2<x){
+            //         if(top3<x){
+            //             x=top3
+            //             console.log('top3')
+            //         } else {
+            //             x=top2
+            //             console.log('top2')
+            //         }
+            //     }else{console.log('top1')}
+        }
+        console.log(minindex)
+        let targetId = targetElement[minindex].id
+        // let list = document.querySelectorAll('.navBar > ul > li > a')
+        // if(list[0].getAttribute('href') === '#'+ targetId)
+        let a = document.querySelector('a[href="#'+targetId+'"]')
+        // {
+        //  a.parentNode.classList.add('active')
+        // }
+        // console.log(top)
+    listenNavBar()
+    }
+}
+function listenNavBar () {
+    let topHead = document.getElementById('topHead')
+    let list = document.querySelectorAll('#topHead .navBar li > a')
 //    console.log(list)
-        if(window.scrollY > 0){
-            topHead.classList.add('navScroll')
-            for(let i =0;i<7;i++){
-                list[i].classList.add('navBarfont')
-            }
-        }else if(window.scrollY==0){
-            topHead.classList.remove('navScroll')
-            for(let i =0;i<7;i++){
-                list[i].classList.remove('navBarfont')
-            }
+    if(window.scrollY > 0){
+        topHead.classList.add('navScroll')
+        for(let i =0;i<7;i++){
+            list[i].classList.add('navBarfont')
+        }
+    }else if(window.scrollY==0){
+        topHead.classList.remove('navScroll')
+        for(let i =0;i<7;i++){
+            list[i].classList.remove('navBarfont')
         }
     }
 }
+
 function listentomouseonnavBar () {
     let list = document.querySelectorAll('.navBar > ul > li')
     for(let i=0;i<list.length;i++){
@@ -55,8 +92,34 @@ function listentomouseonnavBar () {
             let href = x.target.getAttribute('href')
             // console.log(list[i].offsetTop)   //这获取的是navBar离顶部的距离，不可
             let element = document.querySelector(href)   //querySelector和querySelectorAll的区别
-            let y = element.offsetTop
-            window.scrollTo(0, y-70)
+            let y = element.offsetTop          //元素离页面top的距离
+            // console.log(y)
+            scrollAnimation(y)
+
+            // let targetElement = document.querySelectorAll(['data-x'])
+            // console.log(elementtop)
         }
     }
+}
+function scrollAnimation (targettop) {
+    function animate(time) {
+        requestAnimationFrame(animate);
+        TWEEN.update(time);
+    }
+    requestAnimationFrame(animate);
+    let starttop = window.scrollY
+    let currenttop = targettop -70
+    let t = Math.abs((currenttop - starttop)*1.5)
+    let coords = {y: starttop }; // Start at
+    let tween = new TWEEN.Tween(coords) // Create a new tween that modifies 'coords'.
+        // .to({ y: currenttop }, （top/100）*1000) // 到达所有地点的时间都是1s，不能做到全部匀速
+        .to({ y: currenttop }, t)
+        .easing(TWEEN.Easing.Quadratic.InOut) // Use an easing function to make the animation smooth.
+        .onUpdate(function() { // Called after tween.js updates 'coords'.
+            // Move 'box' to the position described by 'coords' with a CSS translation.
+            // console.log(coords.y)
+            window.scrollTo(0, coords.y)
+        })
+        .start(); // Start the tween immediately.
+
 }
